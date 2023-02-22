9d2b486e2d56eb27f7eaabc229655c5dcf3b7a2f
BuildTools <unconfigured@null.spigotmc.org>
2023-02-22 14:02:33 -0500

    Input handling

--- include/window.h
+++ include/window.h
@@ -7,6 +7,9 @@
 
 #include <SDL2/SDL.h>
 
+class IKeyEventListener;
+class IMouseEventListener;
+
 struct Window {
     int WIDTH = 1440;
     int HEIGHT = 720;
@@ -15,6 +18,39 @@ struct Window {
     SDL_Window* window = nullptr;
     
     void handleInput ();
+    static bool keyDown(SDL_Keycode key);
+    static bool keyState(SDL_Keycode key);
+    static bool mouseDown(int button);
+    static bool mouseState(int button);
+    
+    static void registerListener(IKeyEventListener* listener);
+    static void registerListener(IMouseEventListener* listener);
+    static void deleteListener(IKeyEventListener* listener);
+    static void deleteListener(IMouseEventListener* listener);
+};
+
+class IKeyEventListener {
+    public:
+        IKeyEventListener(){
+            Window::registerListener(this);
+        }
+        ~IKeyEventListener(){
+            Window::deleteListener(this);
+        }
+        virtual void keyPressed(SDL_KeyCode key) = 0;
+        virtual void keyReleased(SDL_KeyCode key) = 0;
+};
+
+class IMouseEventListener {
+    public:
+        IMouseEventListener() {
+            Window::registerListener(this);
+        }
+        ~IMouseEventListener() {
+            Window::deleteListener(this);
+        }
+        virtual void mousePressed(int button, int x, int y) = 0;
+        virtual void mouseReleased(int button, int x, int y) = 0;
 };
 
 extern Window window;
--- src/window.cpp
+++ src/window.cpp
@@ -3,9 +3,26 @@
 //
 #include <window.h>
 #include "blt/std/logging.h"
+#include <unordered_map>
+#include <vector>
+#include <algorithm>
+
+std::unordered_map<int, bool> keys {};
+std::unordered_map<int, bool> mouse {};
+
+std::unordered_map<int, bool> key_state {};
+std::unordered_map<int, bool> mouse_state {};
+
+std::vector<IKeyEventListener*> keyListeners;
+std::vector<IMouseEventListener*> mouseListeners;
 
 void Window::handleInput() {
     SDL_Event event;
+    
+    for (const auto& item : key_state)
+        key_state[item.first] = false;
+    for (const auto& button : mouse_state)
+        mouse_state[button.first] = false;
     
     while (SDL_PollEvent(&event)){
         switch (event.type) {
@@ -15,15 +32,65 @@ void Window::handleInput() {
                 std::exit(0);
                 break;
             case SDL_KEYDOWN:
+                for (auto& p : keyListeners)
+                    p->keyPressed(static_cast<SDL_KeyCode>(event.key.keysym.sym));
+                keys[event.key.keysym.sym] = true;
+                key_state[event.key.keysym.sym] = true;
                 break;
             case SDL_KEYUP:
+                for (auto& p : keyListeners)
+                    p->keyReleased(static_cast<SDL_KeyCode>(event.key.keysym.sym));
+                keys[event.key.keysym.sym] = false;
                 break;
             case SDL_MOUSEBUTTONDOWN:
+                for (auto& p : mouseListeners)
+                    p->mousePressed(event.button.button, event.button.x, event.button.y);
+                mouse[event.button.button] = true;
+                mouse_state[event.button.button] = true;
                 break;
             case SDL_MOUSEBUTTONUP:
+                for (auto& p : mouseListeners)
+                    p->mouseReleased(event.button.button, event.button.x, event.button.y);
+                mouse[event.button.button] = false;
                 break;
             default:
                 break;
         }
     }
+}
+
+bool Window::keyDown(SDL_Keycode key) {
+    return keys[(int)key];
+}
+
+bool Window::keyState(SDL_Keycode key) {
+    return key_state[(int)key];
+}
+
+bool Window::mouseDown(int button) {
+    return mouse[button];
+}
+
+bool Window::mouseState(int button) {
+    return mouse_state[button];
+}
+
+void Window::registerListener(IKeyEventListener* listener) {
+    keyListeners.push_back(listener);
+}
+
+void Window::registerListener(IMouseEventListener* listener) {
+    mouseListeners.push_back(listener);
+}
+
+void Window::deleteListener(IKeyEventListener* listener) {
+    auto position = std::find(keyListeners.begin(), keyListeners.end(), listener);
+    if (position != keyListeners.end())
+        keyListeners.erase(position);
+}
+
+void Window::deleteListener(IMouseEventListener* listener) {
+    auto position = std::find(mouseListeners.begin(), mouseListeners.end(), listener);
+    if (position != mouseListeners.end())
+        mouseListeners.erase(position);
 }
